534f426116ef48229943070dee13d43805e9120d
= <=>
2022-10-29 20:24:35 +0200

    removed file

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 use clap::Parser;
+mod valueEmptyCheck;
 
 #[derive(Parser)]
 struct Cli {
-    pattern: String,
+    typeofrequest: String,
     url: String,
 }
 
-fn make_request(url:&str)->String {
-    let body  = reqwest::blocking::get(url) 
-        .unwrap()
-        .text()
-        .unwrap();
+fn make_request(type_reques:&str, url:&str)->String {
+ 
+    let mut body:String =  "string".to_owned();
 
+    if  type_reques ==  "get" {
+    body  = reqwest::blocking::get(url) 
+        .unwrap()
+        .text() 
+        .unwrap();
+    
+    let check_value_empty = valueEmptyCheck::check_if_value_empty::check_if_value_empty([type_reques,url]);
+    
+    if check_value_empty != "None" {
+        body = "check_value_empty".to_owned();
+    }
+
+    if body.is_empty() {
+        body = "Request response body was empty".to_owned();
+    }
+    
+
+
+    }
     format!("Body: {}", body)
 }
+
 
 fn main() {
        let args = Cli::parse();
 
-       let body = make_request(&args.url);
+       let body = make_request(&args.typeofrequest,&args.url);
 
-    
        println!("Body: {}" ,body)
 }       
